2223b80579136facdcbb02f60ec2d630526aa15a
Camila Madureira <ca.madureira094@gmail.com>
2025-03-21 19:38:26 -0300

    feat: create get livros

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import sqlite3  
-from flask import Flask, request, jsonify  
+import sqlite3
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 def init_db():
-   
     with sqlite3.connect("database.db") as conn:
-       
         conn.execute(
             """
                 CREATE TABLE IF NOT EXISTS LIVROS(
@@ -25,34 +23,43 @@ init_db()
 
 @app.route("/doar", methods=["POST"])
 def doar():
-   
     dados = request.get_json()
-    print(f" AQUI ESTÃO OS DADOS RETORNADOS DO CLIENTE {dados}")  
+    print(f" AQUI ESTÃO OS DADOS RETORNADOS DO CLIENTE {dados}")
 
-    
-    titulo = dados.get("titulo")  
-    categoria = dados.get("categoria")  
-    autor = dados.get("autor")  
-    imagem_url = dados.get("imagem_url")  
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    imagem_url = dados.get("imagem_url")
 
-   
     if not titulo or not categoria or not autor or not imagem_url:
-        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400  
-    
- 
+        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
+
     with sqlite3.connect("database.db") as conn:
-        
         conn.execute(f"""
         INSERT INTO LIVROS (titulo, categoria, autor, imagem_url) 
         VALUES ("{titulo}", "{categoria}", "{autor}", "{imagem_url}")
         """)
-    
-    conn.commit() 
 
- 
     return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
 
 
+@app.route("/livros", methods=["GET"])
+def listar_livros():
+    with sqlite3.connect("database.db") as conn:
+        livros = conn.execute("SELECT * FROM LIVROS").fetchall()
+    
+    livros_formatados = []
+
+    for item in livros:
+        dicionario_livros = {
+            "id": item[0],
+            "titulo": item[1],
+            "autor": item[3],
+            "imagem_url": item[4]
+        }
+        livros_formatados.append(dicionario_livros)
+
+    return jsonify(livros_formatados)  
+
 if __name__ == "__main__":
-   
     app.run(debug=True)
